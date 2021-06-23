ee56777a873c2737bf1591b969bc7cffd7139b03
Grinevich Sergey <grinevich.sergey.2001@gmail.com>
2021-06-23 12:56:28 +0300

    cors allow added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const cors = require('cors')
 const mailRouter = require('./api.js');
 
 const app = express();
 app.use('/api', mailRouter);
-app.use(express.static(__dirname + '/templates/styles'));
+app.use(cors());
 
 app.listen(process.env.PORT | 8080);
